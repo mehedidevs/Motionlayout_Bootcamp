299b69b55d8784223536c80d05f80e52bae4786f
mehedidevs <mehedi.hasan.app.dev@gmail.com>
2024-05-15 19:52:35 +0600

    initial commit

---
+++ app/src/main/java/com/mehedi/motionlayoutbootcamp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mehedi.motionlayoutbootcamp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layout_image_filter)
+        
+    }
+}
